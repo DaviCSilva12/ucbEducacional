b61380f0ea7c26cecc5c43cac9cb8ab81eec6eff
gabriel alberto duarte de andrade <166182008+curupiracuzim@users.noreply.github.com>
2025-09-18 23:50:53 -0300

    correção de importaçao e testes

--- UCBEnsino/src/main/java/view/main.java
+++ UCBEnsino/src/main/java/view/main.java
@@ -3,14 +3,26 @@
  */
 
 package view;
-
 /**
  *
  * @author Pichau
  */
-public class main {
+import model.Aluno;
+import model.Curso;
+import model.Disciplina;
+import model.Funcionario;
+import model.Matricula;
+import model.Nota;
+import model.Professor;
+import model.Turma;
+import model.Usuario;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Aluno aluno = new Aluno();
+        Curso curso = new Curso();
+        Usuario usuario = new Usuario();
+        
+        System.out.println("Testando");
     }
 }
